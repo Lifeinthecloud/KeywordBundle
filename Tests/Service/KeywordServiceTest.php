ad0a923a3047554f37224eef31f3c039353b3cbd
Antoine DARCHE <darche.antoine@gmail.com>
2015-01-13 00:15:59 +0100

    Add unit test

---
+++ Tests/Service/KeywordServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Lifeinthecloud\KeywordBundle\Service\KeywordService;
+
+/**
+ * Class KeywordServiceTest
+ *
+ * @package Lifeinthecloud\KeywordBundle\Tests\Service
+ *
+ * @version 1.0
+ * @date 07/01/2015
+ * @author Antoine DARCHE & Grégory DARCHE (Life in the cloud)
+ * @organisation Lifeinthecloud
+ * @url http://lifeinthecloud.fr
+ */
+class KeywordServiceTest {
+
+    /**
+     *
+     */
+    function __construct()
+    {
+        //$keywordService = new KeywordService();
+        //$keywordService->addText();
+    }
+}
